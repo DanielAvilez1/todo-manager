b1755371bcb58639d1a77a8c76eb9265cad53246
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-05-22 21:18:36 -0700

    Adding styling

---
+++ app.js
@@ -0,0 +1,20 @@
+//global vars
+let newTask= document.getElementById("txtTask");
+let count= 0;
+
+function registerTask(){
+    //display on console---> in a html element
+    console.log(newTask.value);
+    let tmp=`<li id="${count}" class="task">${newTask.value} <button onclick="deleteTask(${count})">🗑️</button> </li>`;
+    document.getElementById("todoList").innerHTML+=tmp;
+    clearform();
+    count++;
+
+}
+function deleteTask(x){
+    document.getElementById(x).remove();
+
+}
+function clearform(){
+    newTask.value="";
+}
